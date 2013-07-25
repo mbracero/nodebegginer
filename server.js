c518b5f6d647f238d4498bf0811095e70caad5ad
mbracero <manuel.bracero@gmail.com>
2013-07-25 09:09:10 +0000

    Add server

---
+++ server.js
@@ -0,0 +1,11 @@
+var PORT = 5623;
+
+var http = require('http');
+
+http.createServer(
+	function(req, res) {
+		res.writeHead(200, {"Content-Type": "text/html"});
+    res.write("Hola Mundo !!!");
+    res.end();
+	}
+).listen(PORT);
